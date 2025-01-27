81e418e40deb899abc0678da25c37591277e1873
angelramos <angelgonzalez171104@gmail.com>
2025-01-27 08:23:59 -0600

    Cambios al agregar el toast

--- app/src/main/java/com/example/encuestapreferenciasjava/MainActivity.java
+++ app/src/main/java/com/example/encuestapreferenciasjava/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.encuestapreferenciasjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtColor;
+    EditText txtComida;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtColor = findViewById(R.id.colorInput);
+        txtComida = findViewById(R.id.comidaInput);
+        btnEnviar = findViewById(R.id.enviarButton);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colorFavorito = txtColor.getText().toString();
+                String comidaFavorita = txtComida.getText().toString();
+
+                if (colorFavorito.isEmpty() || comidaFavorita.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(MainActivity.this,
+                            "Color Favorito: " + colorFavorito + "\nComida Favorita: " + comidaFavorita,
+                            Toast.LENGTH_LONG).show();
+
+                    txtColor.setText("");
+                    txtComida.setText("");
+                }
+            }
         });
     }
 }
